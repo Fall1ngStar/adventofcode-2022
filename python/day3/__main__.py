ee87327c17098b58b7f55b1de359fc0ac568f56b
Fall1ngStar <fall1ngstar.public@gmail.com>
2022-12-04 00:21:33 -0500

    feat: add python day3

---
+++ python/day3/__main__.py
@@ -0,0 +1,28 @@
+from lib import chunker, read_input
+
+SCORE = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def part1(input_data):
+    total = 0
+    for line in input_data.splitlines():
+        size = len(line) // 2
+        first, second = line[:size], line[size:]
+        common = {*first} & {*second}
+        total += SCORE.index(common.pop())
+    return total
+
+
+def part2(input_data):
+    total = 0
+    lines = [*input_data.splitlines()]
+    for a, b, c in chunker(lines, 3):
+        common = {*a} & {*b} & {*c}
+        total += SCORE.index(common.pop())
+    return total
+
+
+if __name__ == "__main__":
+    input_data = read_input("day3")
+    print(f"part1 -> {part1(input_data)}")
+    print(f"part2 -> {part2(input_data)}")
--- python/lib.py
+++ python/lib.py
@@ -4,3 +4,7 @@ import os
 def read_input(day, case="input"):
     with open(os.path.join(day, case)) as file:
         return file.read()
+
+
+def chunker(seq, size):
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
