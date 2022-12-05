a607f0e6f7eb5da6084207d14909d4f91c8796c4
Fall1ngStar <fall1ngstar.public@gmail.com>
2022-12-04 23:51:39 -0500

    feat: add python day4

---
+++ python/day4/__main__.py
@@ -0,0 +1,36 @@
+from lib import read_input
+
+
+def part1(input_data):
+    score = 0
+    for line in input_data.splitlines():
+
+        a, b = line.split(",")
+        start_a, end_a = map(int, a.split("-"))
+        start_b, end_b = map(int, b.split("-"))
+
+        if start_a <= start_b and end_b <= end_a:
+            score += 1
+        elif start_b <= start_a and end_a <= end_b:
+            score += 1
+    return score
+
+
+def part2(input_data):
+    score = 0
+    for line in input_data.splitlines():
+
+        a, b = line.split(",")
+        start_a, end_a = map(int, a.split("-"))
+        start_b, end_b = map(int, b.split("-"))
+        range_a = set(range(start_a, end_a + 1))
+        range_b = set(range(start_b, end_b + 1))
+        if range_a & range_b:
+            score += 1
+    return score
+
+
+if __name__ == "__main__":
+    input_data = read_input("day4")
+    print(f"part1 -> {part1(input_data)}")
+    print(f"part2 -> {part2(input_data)}")
