89285f88c0f8e6856815bed6892ca20f60584a22
Fall1ngStar <fall1ngstar.public@gmail.com>
2022-12-23 10:45:46 +0100

    chore: formatting

--- python/day14/__main__.py
+++ python/day14/__main__.py
@@ -4,7 +4,7 @@ from lib import read_input, Point, window
 def parse_input(input_data):
     grid = {}
     for line in input_data.splitlines():
-        coords =  [Point(*map(int, coord.split(","))) for coord in line.split(" -> ")]
+        coords = [Point(*map(int, coord.split(","))) for coord in line.split(" -> ")]
         for start, end in window(coords, 2):
             direction = (end - start).nor
             current = start
@@ -14,29 +14,33 @@ def parse_input(input_data):
                 grid[(*current,)] = "#"
     return grid
 
+
 def print_grid(grid):
-    lowest_y = max([y for _,y in grid.keys()])
-    lowest_x = min([x for (x,_) in grid.keys()])
-    highest_x = max([x for (x,_) in grid.keys()])
-    lines = [[" " for _ in range((highest_x-lowest_x)+1)] for _ in range(lowest_y+1)]
+    lowest_y = max([y for _, y in grid.keys()])
+    lowest_x = min([x for (x, _) in grid.keys()])
+    highest_x = max([x for (x, _) in grid.keys()])
+    lines = [
+        [" " for _ in range((highest_x - lowest_x) + 1)] for _ in range(lowest_y + 1)
+    ]
 
     for (x, y), value in grid.items():
-        lines[y][x-lowest_x] = value
+        lines[y][x - lowest_x] = value
     for line in lines:
         print("".join(line))
     print("_")
 
+
 def part1(input_data):
     grid = parse_input(input_data)
     print_grid(grid)
-    lowest = max([y for _,y in grid.keys()]) + 1
+    lowest = max([y for _, y in grid.keys()]) + 1
     loop = False
     while not loop:
         sand = Point(500, 0)
         grid[(*sand,)] = "o"
         falling = True
         while falling and (*sand,)[1] < lowest:
-            for direction in [Point(0,1), Point(-1,1), Point(1,1)]:
+            for direction in [Point(0, 1), Point(-1, 1), Point(1, 1)]:
                 next_pos = sand + direction
                 if not grid.get((*next_pos,)):
                     del grid[(*sand,)]
@@ -49,20 +53,20 @@ def part1(input_data):
         if falling:
             loop = True
 
-            
     return len([val for val in grid.values() if val == "o"]) - 1
+
 
 def part2(input_data):
     grid = parse_input(input_data)
     print_grid(grid)
-    lowest = max([y for _,y in grid.keys()]) + 1
+    lowest = max([y for _, y in grid.keys()]) + 1
     loop = True
     while loop:
         sand = Point(500, 0)
         grid[(*sand,)] = "o"
         falling = True
         while falling and (*sand,)[1] < lowest:
-            for direction in [Point(0,1), Point(-1,1), Point(1,1)]:
+            for direction in [Point(0, 1), Point(-1, 1), Point(1, 1)]:
                 next_pos = sand + direction
                 if not grid.get((*next_pos,)):
                     del grid[(*sand,)]
--- python/day15/__main__.py
+++ python/day15/__main__.py
@@ -9,12 +9,17 @@ DIRECTIONS = (
     (Point(0, -1), Point(0, 1)),
 )
 
+
 def parse_data(input_data):
     result = []
     for line in input_data.splitlines():
-        xa, ya, xb, yb = re.match(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)", line).groups()
+        xa, ya, xb, yb = re.match(
+            r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
+            line,
+        ).groups()
         result.append((Point(int(xa), int(ya)), Point(int(xb), int(yb))))
     return result
+
 
 def part1(input_data):
     target_line = 2000000
@@ -31,6 +36,7 @@ def part1(input_data):
             impossible_posistions.remove(beacon[0])
     return len(impossible_posistions)
 
+
 # TODO: not working with example input
 def part2(input_data):
     bounds = 4_000_000
@@ -40,28 +46,27 @@ def part2(input_data):
     for sensor, beacon in lines:
         distance = len(sensor - beacon)
         x, y = sensor
-        plus_x_lines.add(-(x-y+distance))
-        plus_x_lines.add(-(x-y-distance))
-        minus_x_lines.add(x+y+distance)
-        minus_x_lines.add(x+y-distance)
+        plus_x_lines.add(-(x - y + distance))
+        plus_x_lines.add(-(x - y - distance))
+        minus_x_lines.add(x + y + distance)
+        minus_x_lines.add(x + y - distance)
     candidates_plus_x = set()
     for a, b in itertools.combinations(plus_x_lines, 2):
-        avg = (a+b)//2
-        if abs(a-b) == 2 and avg not in plus_x_lines:
+        avg = (a + b) // 2
+        if abs(a - b) == 2 and avg not in plus_x_lines:
             candidates_plus_x.add(avg)
     candidates_minus_x = set()
     for a, b in itertools.combinations(minus_x_lines, 2):
-        avg = (a+b)//2
-        if abs(a-b) == 2 and avg not in minus_x_lines:
+        avg = (a + b) // 2
+        if abs(a - b) == 2 and avg not in minus_x_lines:
             candidates_minus_x.add(avg)
 
     for a, b in itertools.product(candidates_minus_x, candidates_plus_x):
-        x = (b-a)// -2
+        x = (b - a) // -2
         y = -x + a
         if 0 <= x <= bounds and 0 <= y <= bounds:
             return x * 4_000_000 + y
 
-        
 
 if __name__ == "__main__":
     input_data = read_input("day15")
